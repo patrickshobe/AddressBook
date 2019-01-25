185a351e5e65c1134104c1434313c9ae8fa27155
Patrick Shobe <patrick@patrickshobe.com>
2019-01-25 16:40:53 -0700

    Initial Commit Closes #1 Closes #2

---
+++ README.md
@@ -0,0 +1,4 @@
+# AddressBook
+
+Address Book with City and State validation for addresses via the USPS API.
+Built using Python, Flask, PostgreSQL
---
+++ root_spec.py
@@ -0,0 +1,15 @@
+from app import app
+
+import unittest
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_index(self):
+        test = app.test_client(self)
+        response = test.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'This is the index')
+
+
+if __name__ == '__main__':
+    unittest.main()
